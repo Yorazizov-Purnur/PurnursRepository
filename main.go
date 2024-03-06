e651db565f8d1dfa12b6cca3bb5af725af3e5ec9
Your Name <you@example.com>
2024-03-06 18:06:04 +0500

    team work

--- main.go
+++ main.go
@@ -2,8 +2,27 @@ package main
 
 import "github.com/gin-gonic/gin"
 
+type Users struct {
+	Name    string
+	Surname string
+	Pasword string
+	Login   string
+}
+
+var UserSlice = []Users{}
+
 func main() {
 	r := gin.Default()
 	r.POST("/getuser")
+
 	r.Run(":1303")
+}
+func Register(c *gin.Context) {
+	var shablon Users
+	if shablon.Login == "" || shablon.Name == "" || shablon.Pasword == "" || shablon.Surname == "" {
+		c.JSON(404, "empty field")
+	} else {
+		c.JSON(200, "succes")
+		UserSlice = append(UserSlice, shablon)
+	}
 }
